695e0e1bc5573845029fb8294438da3fb0d3f8e8
mohammedakbark <ribinrajop@gmail.com>
2025-09-12 18:50:40 +0530

    push notification configration finished both ios and android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,81 @@
 
+// import java.util.Properties
+// import java.io.FileInputStream
+// plugins {
+//     id("com.android.application")
+//     // START: FlutterFire Configuration
+//     id("com.google.gms.google-services")
+//     // END: FlutterFire Configuration
+//     id("kotlin-android")
+//     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+//     id("dev.flutter.flutter-gradle-plugin")
+// }
+
+// val keystoreProperties = Properties()
+// val keystorePropertiesFile = rootProject.file("key.properties")
+// if (keystorePropertiesFile.exists()) {
+//     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+// }
+
+// android {
+//     namespace = "com.example.wheels_kart"
+//     compileSdk = flutter.compileSdkVersion
+//     ndkVersion = "27.0.12077973"
+
+//     compileOptions {
+//         sourceCompatibility = JavaVersion.VERSION_11
+//         targetCompatibility = JavaVersion.VERSION_11
+//     }
+
+//     kotlinOptions {
+//         jvmTarget = JavaVersion.VERSION_11.toString()
+//     }
+
+//     defaultConfig {
+//         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+//         applicationId = "com.crisant.wheelskart"
+//         // You can update the following values to match your application needs.
+//         // For more information, see: https://flutter.dev/to/review-gradle-config.
+//         minSdk = flutter.minSdkVersion
+//         targetSdk = flutter.targetSdkVersion
+//         versionCode = flutter.versionCode
+//         versionName = flutter.versionName
+//     }
+
+//     signingConfigs {
+//         create("release") {
+//             keyAlias = keystoreProperties["keyAlias"] as String
+//             keyPassword = keystoreProperties["keyPassword"] as String
+//             storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+//             storePassword = keystoreProperties["storePassword"] as String
+//         }
+//     }
+
+//     buildTypes {
+//         release {
+         
+//             signingConfig = signingConfigs.getByName("release")
+//         }
+//     }
+// }
+
+// flutter {
+//     source = "../.."
+// }
+// dependencies {
+//     implementation("com.google.android.material:material:1.12.0")
+// }
+
+
+
+
 import java.util.Properties
 import java.io.FileInputStream
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -19,9 +92,13 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
+    // **Enable core library desugaring and Java 11 compatibility**
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+
+        // Enable core library desugaring (Kotlin DSL flag)
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -33,7 +110,7 @@ android {
         applicationId = "com.crisant.wheelskart"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -50,7 +127,7 @@ android {
 
     buildTypes {
         release {
-         
+
             signingConfig = signingConfigs.getByName("release")
         }
     }
@@ -59,6 +136,9 @@ android {
 flutter {
     source = "../.."
 }
+
+// --- dependencies block: add coreLibraryDesugaring dependency ---
 dependencies {
     implementation("com.google.android.material:material:1.12.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
